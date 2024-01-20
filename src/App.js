def53b95467d2ce62e512e20aa1b8bc1c3aafa8b
Manish patodiya <mpatodiya2@gmail.com>
2024-01-20 19:47:36 +0530

    make three section in the page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Container } from "react-bootstrap";
+// import App from './App';
+import { Header, Card1, Card2, Card3 } from "./code/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Header />
+      <Card1 />
+      <Card2 />
+      <Card3 />
+    </Container>
   );
 }
 
